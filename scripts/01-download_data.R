85a782d63153e96a2a2ae7ec1cc19a21c5bfd1f9
Tam Ly <annatn.ly@mail.utoronto.ca>
2024-03-05 08:39:54 +0000

    Added distribution of prices

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,50 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Purpose: Downloads and saves the data from Airbnb
+# Author: Tam Ly
+# Date: 5 March 2024
+# Contact: annatn.ly@mail.utoronto.ca
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+url <-
+  paste0(
+    "http://data.insideairbnb.com/france/ile-de-france/paris/2023-12-12/data/listings.csv.gz"
+  )
 
-
+airbnb_data <-
+  read_csv(
+    file = url,
+    guess_max = 20000
+  )
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(airbnb_data, "airbnb_data.csv")
 
-         
+
+#### Create a parquet file with selected variables ####
+airbnb_data_selected <-
+  airbnb_data |>
+  select(
+    host_id,
+    host_response_time,
+    host_is_superhost,
+    host_total_listings_count,
+    neighbourhood_cleansed,
+    bathrooms,
+    bedrooms,
+    price,
+    number_of_reviews,
+    review_scores_rating,
+    review_scores_accuracy,
+    review_scores_value
+  )
+
+write_parquet(
+  x = airbnb_data_selected, 
+  sink = 
+    "2024-12-12-paris-airbnblistings-select_variables.parquet"
+)
+
+rm(airbnb_data)
